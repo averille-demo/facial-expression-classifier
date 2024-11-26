d011197795d6a1882f396a1a4a4aa8c8a307a74a
averille-demo <cloud.apps@averille.dev>
2024-11-26 13:14:20 -0800

    add command line args and model settings

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # facial-expression-classifier
-DEMO: facial expression recognition with Keras model
+DEMO: facial expression recognition with Keras model and pydantic settings
 
 [![Python 3.12](https://img.shields.io/badge/python-3.12-blue.svg)](https://www.python.org/downloads/release/python-312/)
 [![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://opensource.org/licenses/MIT)
 [![pre-commit](https://img.shields.io/badge/pre--commit-enabled-blue?logo=pre-commit&logoColor=white)](https://github.com/pre-commit/pre-commit)
 [![ruff](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/astral-sh/ruff/main/assets/badge/v2.json)](https://github.com/astral-sh/ruff)
 [![uv](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/astral-sh/uv/main/assets/badge/v0.json)](https://github.com/astral-sh/uv)
-
 
 ## [UV](https://docs.astral.sh/uv/) Setup
 ```
@@ -19,4 +18,14 @@ uv lock
 uv sync --refresh
 uv run pre-commit autoupdate
 uv run pre-commit install --install-hooks
+```
+
+### [FER-2013 dataset](https://www.kaggle.com/datasets/msambare/fer2013/code)
+Consists of 48x48 pixel grayscale images of faces that have been automatically registered to be centred and occupy a similar amount of space in each image.
+The dataset contains 24400 images, with 22968 examples in the training set and 1432 examples in public test set.
+one of seven categories (0=Angry, 1=Disgust, 2=Fear, 3=Happy, 4=Sad, 5=Surprise, 6=Neutral)
+
+### Train Model:
+```
+uv run python src/facial_expressions_classifier/run.py --epochs 10 --batch_size 30 --train_model
 ```
--- src/facial_expression_classifier/run.py
+++ src/facial_expression_classifier/run.py
@@ -1,9 +1,22 @@
 """Pipeline to create augmented data and train sequential model for facial expression classification."""
 
+import json
+
 import click
 import pendulum
 import structlog
-from util.settings import MAX_BATCH_SIZE, MAX_EPOCHS, MIN_BATCH_SIZE, MIN_EPOCHS, ModelSettings
+from keras.src.layers import BatchNormalization, Conv2D, Dense, Dropout, Flatten, Input, MaxPooling2D
+from keras.src.legacy.preprocessing.image import ImageDataGenerator
+from keras.src.models.sequential import Sequential
+from keras.src.optimizers import Adam
+
+from facial_expression_classifier.util.settings import (
+    MAX_BATCH_SIZE,
+    MAX_EPOCHS,
+    MIN_BATCH_SIZE,
+    MIN_EPOCHS,
+    ModelSettings,
+)
 
 structlog.configure(
     cache_logger_on_first_use=True,
@@ -16,12 +29,134 @@ structlog.configure(
 )
 
 
+def build_model(
+    settings: ModelSettings,
+) -> Sequential:
+    """Setup sequential model with Rectified Linear Unit (ReLU) activation function."""
+    model = Sequential(name=settings.model_name)
+    model.add(Input(shape=(settings.image_size, settings.image_size, 1)))
+
+    model.add(Conv2D(filters=32, kernel_size=settings.kernel_size, activation=settings.activation))
+    model.add(BatchNormalization())
+    model.add(Conv2D(filters=64, kernel_size=settings.kernel_size, activation=settings.activation))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=settings.pool_size))
+    model.add(Dropout(rate=settings.drop_out_rate))
+
+    model.add(Conv2D(filters=128, kernel_size=settings.kernel_size, activation=settings.activation))
+    model.add(BatchNormalization())
+    model.add(Conv2D(filters=128, kernel_size=settings.kernel_size, activation=settings.activation))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=settings.pool_size))
+    model.add(Dropout(rate=settings.drop_out_rate))
+
+    model.add(Conv2D(filters=256, kernel_size=settings.kernel_size, activation=settings.activation))
+    model.add(BatchNormalization())
+    model.add(Conv2D(filters=256, kernel_size=settings.kernel_size, activation=settings.activation))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=settings.pool_size))
+    model.add(Dropout(rate=settings.drop_out_rate))
+
+    model.add(Flatten())
+    model.add(Dense(units=256, activation=settings.activation))
+    model.add(BatchNormalization())
+    model.add(Dropout(rate=settings.drop_out_rate))
+    model.add(Dense(units=7, activation="softmax"))
+
+    model.compile(
+        loss="categorical_crossentropy",
+        optimizer=Adam(learning_rate=0.0001),
+        metrics=["accuracy"],
+    )
+    return model
+
+
+def init_training_data(
+    settings: ModelSettings,
+) -> ImageDataGenerator:
+    """Setup training dataset with augmentation."""
+    # remove prior augmented images
+    for to_purge in settings.aug_img_folder.joinpath("train").glob("*.png"):
+        to_purge.unlink()
+    # setup augmentation parameters
+    train_datagen = ImageDataGenerator(
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        horizontal_flip=True,
+        rescale=1.0 / 255,
+        validation_split=settings.validation_split,
+    )
+    train_generator = train_datagen.flow_from_directory(
+        directory=settings.src_img_folder.joinpath("train"),
+        target_size=(settings.image_size, settings.image_size),
+        batch_size=settings.batch_size,
+        shuffle=True,
+        color_mode="grayscale",
+        class_mode="categorical",
+        subset="training",
+        keep_aspect_ratio=False,
+        # save_to_dir=settings.aug_img_folder.joinpath("train"),
+    )
+    log.info(f"training image count: {len(train_generator)}")
+    return train_generator
+
+
+def init_validation_data(
+    settings: ModelSettings,
+) -> ImageDataGenerator:
+    """Setup validation dataset without augmentation."""
+    # remove prior augmented images
+    for to_purge in settings.aug_img_folder.joinpath("test").glob("*.png"):
+        to_purge.unlink()
+    # setup augmentation parameters
+    validation_datagen = ImageDataGenerator(
+        rescale=1.0 / 255,
+        validation_split=settings.validation_split,
+    )
+    validation_generator = validation_datagen.flow_from_directory(
+        directory=settings.src_img_folder.joinpath("test"),
+        target_size=(settings.image_size, settings.image_size),
+        batch_size=settings.batch_size,
+        shuffle=True,
+        color_mode="grayscale",
+        class_mode="categorical",
+        subset="validation",
+        keep_aspect_ratio=False,
+        # save_to_dir=settings.aug_img_folder.joinpath("test"),
+    )
+    log.info(f"validation image count: {len(validation_generator)}")
+    return validation_generator
+
+
+def fit_model(
+    model: Sequential,
+    settings: ModelSettings,
+    train_generator: ImageDataGenerator,
+    validation_generator: ImageDataGenerator,
+):
+    """Train model and save results to file."""
+    # train model
+    trained_model = model.fit(
+        x=train_generator,
+        epochs=settings.epochs,
+        validation_data=validation_generator,
+        # callbacks=[checkpoint_callback],
+    )
+    # save history as '.json' file
+    settings.history_file.write_text(data=json.dumps(trained_model.history, indent=2, default=str), encoding="utf-8")
+    settings.summary_file.write_text(data=model.to_json(indent=2), encoding="utf-8")
+    # save model as '.keras' file
+    model.save(settings.model_file, overwrite=True)
+    log.info(f"saved: {settings.model_file.name}")
+
+
+# pylint: disable=no-value-for-parameter
 @click.command()
 @click.option(
     "--epochs",
     "-e",
     show_default=True,
-    default=60,
+    default=1,
     help=f"choose number of epochs between {MIN_EPOCHS} and {MAX_EPOCHS}",
 )
 @click.option(
@@ -36,27 +171,30 @@ structlog.configure(
     "-t",
     is_flag=True,
     show_default=True,
-    default=False,
+    default=True,
     help="apply boolean flag to train model",
-)
-@click.option(
-    "--plot-history",
-    "-p",
-    is_flag=True,
-    show_default=True,
-    default=False,
-    help="apply boolean flag to plot model training history",
 )
 def trigger_pipeline(
     epochs: int,
     batch_size: int,
     train_model: bool,
-    plot_history: bool,
 ):
     """Driver for pipeline."""
     settings = ModelSettings(epochs=epochs, batch_size=batch_size, version="1.0.0")
-    print(f"starting: '{settings.model_name}' {train_model=} {plot_history=}")
+    print(f"starting: '{settings.model_name}' {train_model=}")
     print(f"settings: {settings.model_dump()}")
+
+    train_generator = init_training_data(settings=settings)
+    validation_generator = init_validation_data(settings=settings)
+
+    if train_model:
+        model = build_model(settings=settings)
+        fit_model(
+            model=model,
+            settings=settings,
+            train_generator=train_generator,
+            validation_generator=validation_generator,
+        )
 
     print(f"completed: '{settings.model_name}' {pendulum.now().to_datetime_string()}")
 
